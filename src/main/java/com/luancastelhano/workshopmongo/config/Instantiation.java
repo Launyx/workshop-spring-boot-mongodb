d2b4cdcdee837e398f1da93361ac2fc1fd097184
Launyx <lunyx.castelhano@gmail.com>
2024-07-02 17:56:41 -0300

    base data instantiation operation

---
+++ src/main/java/com/luancastelhano/workshopmongo/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.luancastelhano.workshopmongo.config;
+
+import com.luancastelhano.workshopmongo.domain.User;
+import com.luancastelhano.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        // Para zerar a coleção no mongoDB
+        userRepository.deleteAll();
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
