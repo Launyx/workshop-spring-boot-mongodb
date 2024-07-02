561f294e4e3d4bdeb6858ce8838784b66540437b
Launyx <lunyx.castelhano@gmail.com>
2024-07-02 18:56:45 -0300

    Finding user by id

--- src/main/java/com/luancastelhano/workshopmongo/resources/UserResource.java
+++ src/main/java/com/luancastelhano/workshopmongo/resources/UserResource.java
@@ -3,6 +3,7 @@ package com.luancastelhano.workshopmongo.resources;
 import com.luancastelhano.workshopmongo.dto.UserDTO;
 import com.luancastelhano.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luancastelhano.workshopmongo.domain.User;
@@ -28,5 +29,12 @@ public class UserResource {
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).toList();
 
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @GetMapping(value = "/{id}")
+    // Annotation @PathVariable para indicar que o id passado como parâmetro deve casar com o id passado na URI
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 }
--- src/main/java/com/luancastelhano/workshopmongo/services/UserService.java
+++ src/main/java/com/luancastelhano/workshopmongo/services/UserService.java
@@ -3,10 +3,12 @@ package com.luancastelhano.workshopmongo.services;
 import com.luancastelhano.workshopmongo.domain.User;
 import com.luancastelhano.workshopmongo.repository.UserRepository;
 
+import com.luancastelhano.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +18,12 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = repo.findById(id);
+        // Retorna o objeto(usuário) com o id providenciado, caso tal não exista, é lançada a exceção customizada
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+
     }
 }
