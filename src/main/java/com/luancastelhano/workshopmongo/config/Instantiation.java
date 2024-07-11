42022ffb8182d8d0e37f8c6c8aac5a58f1378fe6
Launyx <lunyx.castelhano@gmail.com>
2024-07-11 17:31:01 -0300

    Adding comments to the posts

--- src/main/java/com/luancastelhano/workshopmongo/config/Instantiation.java
+++ src/main/java/com/luancastelhano/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.luancastelhano.workshopmongo.config;
 import com.luancastelhano.workshopmongo.domain.Post;
 import com.luancastelhano.workshopmongo.domain.User;
 import com.luancastelhano.workshopmongo.dto.AuthorDTO;
+import com.luancastelhano.workshopmongo.dto.CommentDTO;
 import com.luancastelhano.workshopmongo.repository.PostRepository;
 import com.luancastelhano.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,8 @@ public class Instantiation implements CommandLineRunner {
         // Para zerar a coleção no mongoDB
         userRepository.deleteAll();
         postRepository.deleteAll();
+
+
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
@@ -39,6 +42,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
