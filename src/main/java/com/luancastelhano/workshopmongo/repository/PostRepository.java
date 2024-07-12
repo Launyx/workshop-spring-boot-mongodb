662430d71f501ea82049fbcc99d5845b7592d785
Launyx <lunyx.castelhano@gmail.com>
2024-07-12 15:13:18 -0300

    Simple consult with @query

--- src/main/java/com/luancastelhano/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/luancastelhano/workshopmongo/repository/PostRepository.java
@@ -2,14 +2,23 @@ package com.luancastelhano.workshopmongo.repository;
 
 import com.luancastelhano.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    // "?0" indica o comando jSON que o primeiro parâmetro do método será utilizado
+    @Query("{ title: { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
     // Query method para buscar um "Title" (nome do atributo desejado), que contenha o texto passado como parâmetro
     // Ignore case para ignorar diferença entre letras maiúsculas e minúsculas
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [ {date: {$gte: ?1}, {date: {$lte: ?2} }, {} ]")
+    List<Post> fullSearch(String text, Date minDate, Date maxData);
 }
