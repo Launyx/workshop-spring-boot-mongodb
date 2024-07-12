1d4bf8188e242a3e8286a42ace98762335ff76c0
Launyx <lunyx.castelhano@gmail.com>
2024-07-12 15:51:45 -0300

    Consult with many criteria

--- src/main/java/com/luancastelhano/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/luancastelhano/workshopmongo/repository/PostRepository.java
@@ -19,6 +19,6 @@ public interface PostRepository extends MongoRepository<Post, String> {
     // Ignore case para ignorar diferença entre letras maiúsculas e minúsculas
     List<Post> findByTitleContainingIgnoreCase(String text);
 
-    @Query("{ $and: [ {date: {$gte: ?1}, {date: {$lte: ?2} }, {} ]")
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
     List<Post> fullSearch(String text, Date minDate, Date maxData);
 }
